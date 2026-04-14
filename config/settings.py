3e20f591c90397d191c987a62c6eeadbf484a482
Home3madison <vadimbruhoveckij2@gmail.com>
2026-04-14 19:37:23 +0600

    Fix media/static absolute URLs for production

--- config/settings.py
+++ config/settings.py
@@ -88,12 +88,12 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'geo' / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = Path(os.getenv('DJANGO_MEDIA_ROOT', BASE_DIR / 'media'))
 
 LOGIN_URL = 'login'
